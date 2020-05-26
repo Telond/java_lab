f65815c4f947a632abdbb3cd279ce1735a287a8a
Telondarasdsad <igor_fomchenko322@mail.ru>
2020-05-26 14:31:11 +0300

    version 1.0

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new GUI());
+
+    }
+}
